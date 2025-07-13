92e8ab3e08c68839d2c669e06f3a4c359d493be9
MerujSargsyan <sargsyanmeruzhan48@gmail.com>
2025-07-13 11:22:11 -0700

    project set up

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CPP_SimpleTest
 A header only, simple testing helper for all projects.
+
+## Usage:
+
+```cpp
+#include "SimpleTester.hpp"
+
+int test_entry() {
+    ST::Tester theTester{}
+}
+```
---
+++ examples/toTerminal/simple_test.cpp
@@ -0,0 +1,32 @@
+#include <SimpleTester.hpp>
+#include <iostream>
+
+bool passingTest1() {
+    return 5 == 5;
+}
+
+bool passingTest2() {
+    return true != false;
+}
+
+bool failingTest3() {
+    return true == false;
+}
+
+bool inputTest(int a, int b) {
+    return a == b;
+}
+
+int main(void) {
+    std::cout << "Starting Testing\n";
+
+    ST::Tester theTester{"Simple Tester"};
+    theTester.registerTest("Test1", passingTest1);
+    theTester.registerTest("Test2", passingTest2);
+    theTester.registerTest("Test3", failingTest3);
+    theTester.registerTest("Test4", inputTest, 10, 10);
+
+    theTester.runTests();
+
+    return 0;
+}
